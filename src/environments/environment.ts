174e9545faf2a8110d573d3435edd3acdc05cf1a
frankwmm <frankwilsonknow@gmail.com>
2021-06-27 16:34:41 -0500

    WGC up M/I tt

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,8 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  url: "http://localhost:8000/api/",
-  production: true,
+  production: false,
 };
 
 /*
